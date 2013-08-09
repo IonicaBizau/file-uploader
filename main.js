986055cbca089394160f8be6dd8fb125c6506b26
Ionica Bizau <bizauionica@yahoo.com>
2013-08-09 11:54:22 +0300

    Add module content.

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+M.wrap('github/IonicaBizau/file-uploader/dev/main.js', function (require, module, exports) {
 var Bind = require("github/jillix/bind");
 var Events = require("github/jillix/events");
 
@@ -6,5 +7,76 @@ module.exports = function(config) {
     var self = this;
     Events.call(self, config);
 
-    // TODO
+    $("form", self.dom).on("submit", function () {
+        var oldHtml = $("iframe").contents().find("pre").html();
+
+        // TODO Create dynamic iframes, instead of using intervals
+        var interval = setInterval(function () {
+            var newHtml = $("iframe").contents().find("pre").html();
+
+            // html was changed
+            if (newHtml !== oldHtml && newHtml) {
+                oldHtml = newHtml;
+                self.emit("fileId", newHtml);
+                clearInterval(interval);
+            }
+        }, 100);
+    });
+
+    self.emit("ready", config);
 };
+
+// function fileUpload(form, action_url, div_id) {
+//     // Create the iframe...
+//     var iframe = document.createElement("iframe");
+//     iframe.setAttribute("id", "upload_iframe");
+//     iframe.setAttribute("name", "upload_iframe");
+//     iframe.setAttribute("width", "0");
+//     iframe.setAttribute("height", "0");
+//     iframe.setAttribute("border", "0");
+//     iframe.setAttribute("style", "width: 0; height: 0; border: none;");
+//
+//     // Add to document...
+//     form.parentNode.appendChild(iframe);
+//     window.frames['upload_iframe'].name = "upload_iframe";
+//
+//     iframeId = document.getElementById("upload_iframe");
+//
+//     // Add event...
+//     var eventHandler = function () {
+//
+//             if (iframeId.detachEvent) iframeId.detachEvent("onload", eventHandler);
+//             else iframeId.removeEventListener("load", eventHandler, false);
+//
+//             // Message from server...
+//             if (iframeId.contentDocument) {
+//                 content = iframeId.contentDocument.body.innerHTML;
+//             } else if (iframeId.contentWindow) {
+//                 content = iframeId.contentWindow.document.body.innerHTML;
+//             } else if (iframeId.document) {
+//                 content = iframeId.document.body.innerHTML;
+//             }
+//
+//             document.getElementById(div_id).innerHTML = content;
+//
+//             // Del the iframe...
+//             setTimeout('iframeId.parentNode.removeChild(iframeId)', 250);
+//         }
+//
+//     if (iframeId.addEventListener) iframeId.addEventListener("load", eventHandler, true);
+//     if (iframeId.attachEvent) iframeId.attachEvent("onload", eventHandler);
+//
+//     // Set properties of form...
+//     form.setAttribute("target", "upload_iframe");
+//     form.setAttribute("action", action_url);
+//     form.setAttribute("method", "post");
+//     form.setAttribute("enctype", "multipart/form-data");
+//     form.setAttribute("encoding", "multipart/form-data");
+//
+//     // Submit the form...
+//     form.submit();
+//
+//     document.getElementById(div_id).innerHTML = "Uploading...";
+// }
+
+return module; });
--- operations.js
+++ operations.js
@@ -1,3 +1,63 @@
+var fs = require("fs");
+
 exports.upload = function (link) {
-    //TODO
+
+    if (!link.files || !link.files.file) {
+        return link.send(400, { error: "Invalid upload" });
+    }
+
+    if (!link.params || !link.params.uploadDir || !link.params.dsUpload) {
+        return link.send(400, { error: "Missing params: uploadDir or dsUpload." });
+    }
+
+    var uploadDir = M.app.getPath() + "/" + link.params.uploadDir;
+    var uploadedFilePath = M.app.getPath() + "/" + link.files.file.path;
+
+    var fileExt = link.files.file.name;
+    fileExt = fileExt.substring(fileExt.lastIndexOf(".")) || "";
+
+    var generatedId = uploadedFilePath.substring(uploadedFilePath.lastIndexOf("/") + 1);
+    var newFilePath = uploadDir + "/" + generatedId + fileExt;
+
+    getCollection(link.params.dsUpload, function (err, collection) {
+
+        console.log("got col");
+
+        if (err) { return link.send(400, err); }
+
+        var docToInsert = {
+            fileName: link.files.file.name,
+            extension: fileExt,
+            filePath: newFilePath,
+            id: generatedId
+        };
+
+        collection.insert(docToInsert, function (err, doc) {
+            if (err) { return link.send(400, err); }
+
+            doc = doc[0];
+            fs.rename(uploadedFilePath, newFilePath, function (err) {
+                if (err) { return link.send(400, err); }
+
+                link.send(200, doc.id);
+            });
+        });
+    });
 };
+
+// private functions
+function getCollection (paramsDs, callback) {
+    M.datasource.resolve(paramsDs, function(err, ds) {
+        if (err) { return callback(400, err); }
+
+        M.database.open(ds, function(err, db) {
+            if (err) { return callback(400, err); }
+
+            db.collection(ds.collection, function(err, collection) {
+                if (err) { return callback(400, err); }
+
+                callback(null, collection);
+            });
+        });
+    });
+}
